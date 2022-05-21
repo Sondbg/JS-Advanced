1bad73bc82f29b33fce350023ad876f440978b6b
Daniel Petkov <sondbg@abv.bg>
2022-05-21 23:59:02 +0300

    StringLength

---
+++ Syntax,Functions and Statements/2.StringLength/StringLength.js
@@ -0,0 +1,5 @@
+function calculateString(a,b,c){
+console.log(a.length+b.length+c.length+`\n`+Math.floor((a.length+b.length+c.length)/3));
+}
+calculateString('chocolate', 'ice cream', 'cake')
+calculateString('pasta', '5', '22.3')
