4a66d7dac3a28f137f978b392f2137dc08cd67a6
Daniel Petkov <sondbg@abv.bg>
2022-06-21 18:09:16 +0300

    Objects exercise

---
+++ Objects/1.CityRecord/CityRecord.js
@@ -0,0 +1,11 @@
+function cityRecord(name,population,treasury){
+let record={};
+record.name=name;
+record.population=population;
+record.treasury=treasury;
+return record
+}
+cityRecord('Tortuga',
+7000,
+15000
+)
